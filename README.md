0be8eb2d69b917500c214d435f661d09470188f1
zakiya <zakiya@users.noreply.github.com>
2021-10-12 13:53:01 -0700

    Adding login page and some basic instructions to get started.

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # odi-publishing-test
 
 `npx playwright install`
+
 `npm run test`
+
+### Testing Drought
+
+- Set paths in terminal (optional)
+
+  `wtg=~/Sites/wordpress-to-github`
+
+  `odt=~/Sites/odi-publishing-test`
+
+- Change [endpoints.json line 34](https://github.com/cagov/wordpress-to-github/blob/main/WordpressSync/endpoints.json#L34) to `"enabledLocal": true,`
+
+  `code $wtg/WordpressSync/endpoints.json`
+
+- Copy examples/local.settings.json from this repo to wordpress to github and change values as needed.
+
+  `cp $odt/examples/example.local.settings.json $wtg/local.settings.json`
+
+- Copy .env and change values to your own WordPress username and password.
+
+  `cp $odt/examples/example.env $odt.env` and change values.
--- tests/publish.spec.js
+++ tests/publish.spec.js
@@ -1,25 +1,24 @@
 const { test, expect } = require("@playwright/test");
 
+// @todo Move to separate file.
 const config = {
-  wp_url: "https://dev-drought-ca-gov.pantheonsite.io/",
+  wp_url: "https://dev-drought-ca-gov.pantheonsite.io",
+  wp_login_path: "/wp-login.php",
+};
+
+const login = () => {
+  console.log("--login");
 };
 
 test("publish", async ({ page }) => {
-  console.log(config);
-  await page.goto(config.wp_url);
-
-  // Login script
-  // login();
+  await page.goto(config.wp_url + config.wp_login_path);
+  login();
 
   // Go to edit environment
-
   // update page
   // create an incrementatl  variable to add to page
-
   // save page
-
   // go to prod
-
   // see variable on page
   // record time to change appears.
 });
