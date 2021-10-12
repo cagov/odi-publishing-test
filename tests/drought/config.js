18dfba31ecb4bda66bdb0a2b91fcea1fb69c4875
zakiya <zakiya@users.noreply.github.com>
2021-10-12 16:55:21 -0700

    Login.

--- tests/drought/config.js
+++ tests/drought/config.js
@@ -1,6 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 const config = {
   wp_url: "https://dev-drought-ca-gov.pantheonsite.io",
   wp_login_path: "/wp-login.php",
+  selector_login: "#user_login",
+  selector_pass: "#user_pass",
+  username: process.env.UN,
+  password: process.env.PW,
 };
 
 export default config;
--- tests/publish.spec.js
+++ tests/publish.spec.js
@@ -2,8 +2,11 @@ import { test, expect } from "@playwright/test";
 import config from "./drought/config.js";
 
 test("publish", async ({ page }) => {
+  // Login.
   await page.goto(config.wp_url + config.wp_login_path);
-
+  await page.fill(config.selector_login, config.username);
+  await page.fill(config.selector_pass, config.password);
+  await page.click("[type=Submit]");
   // Go to edit environment
   // update page
   // create an incrementatl  variable to add to page
