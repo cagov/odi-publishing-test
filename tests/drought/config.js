f4a33898de86a2cce0e643a4075742c3e395018d
zakiya <zakiya@users.noreply.github.com>
2021-10-12 15:19:05 -0700

    Move config to a separate file.

--- tests/drought/config.js
+++ tests/drought/config.js
@@ -1,10 +1,6 @@
-let config = {
-  wp_url: "https://dev-drought-ca-gov.pantheonsite.io/",
+const config = {
+  wp_url: "https://dev-drought-ca-gov.pantheonsite.io",
+  wp_login_path: "/wp-login.php",
 };
 
-// const config = () => {
-//   console.log("");
-// };
-console.log(config);
-// export default config;
-module.exports = config;
+export default config;
--- tests/publish.spec.js
+++ tests/publish.spec.js
@@ -1,18 +1,8 @@
-const { test, expect } = require("@playwright/test");
-
-// @todo Move to separate file.
-const config = {
-  wp_url: "https://dev-drought-ca-gov.pantheonsite.io",
-  wp_login_path: "/wp-login.php",
-};
-
-const login = () => {
-  console.log("--login");
-};
+import { test, expect } from "@playwright/test";
+import config from "./drought/config.js";
 
 test("publish", async ({ page }) => {
   await page.goto(config.wp_url + config.wp_login_path);
-  login();
 
   // Go to edit environment
   // update page
