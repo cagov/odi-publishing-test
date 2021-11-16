21a5351b00504fd41554285d689a8c903eca6097
zakiya <zakiya@users.noreply.github.com>
2021-11-15 18:36:56 -0800

    cagov/odi-engineering#65 Correct video config.

--- playwright.config.js
+++ playwright.config.js
@@ -1,12 +1,12 @@
 const config = {
   retries: 2,
-  video: 'on',
   timeout: 180000,
   use: {
     headless: true,
     viewport: { width: 1020, height: 1720 },
     ignoreHTTPSErrors: false,
     trace: 'on',
+    video: 'on',
   },
 };
 
